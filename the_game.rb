700a9da19af43180b70ddcccc41e6c662829554e
Anthematics <jasonharder2@gmail.com>
2017-08-17 19:33:50 -0400

    v2.0 got battle , levelling up and treasure to work - also added level up method to allow levels to add up

--- the_game.rb
+++ the_game.rb
@@ -1,16 +1,3 @@
-# Create a class called Player.
-# Every player should have four attributes: gold_coins, health_points, and lives.
-# lives should start at 5.
-# gold_coins should start at 0.
-# health_points should start at 10.
-# Your class should have an instance method called level_up that increases lives by one.
-# Your class should have an instance method called collect_treasure that increases gold_coins by one.
-# If gold_coins is a multiple of ten
- # (eg, 10, 20, 30, and so on) then the collect_treasure method should run the level_up method.
-# Your class should have an instance method called do_battle that accepts one damage argument and subtracts it from the player's
-# health_points. If health_points falls below one, subtract one from lives and reset health_points to ten. If you have run out of lives,
-# this method should run another method called restart (see below).
-# The restart instance method should set all attributes back to their starting values (5, 0, and 10).
 require 'pry'
 	class Player
 @@playerlist = []
@@ -20,6 +7,7 @@ require 'pry'
 			@gold_coins= 0
 			@health_points =10
 			@lives =5
+			@level=1
 		end
 
 		def name
@@ -38,12 +26,14 @@ require 'pry'
 
 		def level_up
 			@lives +1
+			@level=@level+1
 		end
 
 		def collect_treasure
 			@gold_coins +=1
-			if @gold_coins *10
+			if @gold_coins % 10 == 0
 				level_up
+
 			end
 		end
 
@@ -82,10 +72,18 @@ matthew=Player.new("matthew")
 puts jason.inspect
 	jason.collect_treasure
 	jason.collect_treasure
-	jason.do_battle
- puts jason.inspect
-jason.do_battle
-puts jason.inspect
-jason.do_battle
-jason.do_battle
+	jason.collect_treasure
+	jason.collect_treasure
+	jason.collect_treasure
+	jason.collect_treasure
+	jason.collect_treasure
+	jason.collect_treasure
+	jason.collect_treasure
+	jason.collect_treasure
+	jason.collect_treasure
+	jason.collect_treasure
+	jason.collect_treasure
+	jason.collect_treasure
+	jason.collect_treasure
+
 puts jason.inspect
